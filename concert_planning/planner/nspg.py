3e64b72405d5e465817b098b7468d143dd40fd82
EdoardoRomiti <edoardo.romiti@iit.it>
2023-08-01 17:35:28 +0200

    nspg account for user specified joint limits (at the ik/opensot level)

--- concert_planning/planner/nspg.py
+++ concert_planning/planner/nspg.py
@@ -6,14 +6,15 @@ from cartesian_interface.pyci_all import *
 import yaml
 
 class GoalSampler:
-    def __init__(self, model, dynamic_links, static_links, validity_check):
-        ik_str = self._generate_ik_cfg(dynamic_links + static_links)
+    def __init__(self, model, dynamic_links, static_links, validity_check, qmin, qmax):
+        self.model = model
+        self.vc = validity_check
+        ik_str = self._generate_ik_cfg(dynamic_links + static_links, qmin, qmax)
         self.ci = pyci.CartesianInterface.MakeInstance('OpenSot', ik_str, model, 1.)
         self.rosapi = pyci.RosServerClass(self.ci, 'planner', 'planner', True)
         self.ik = PositionCartesianSolver(self.ci)
-        self.model = model
-        self.vc = validity_check
         self._nspg = NSPG.GoalSamplerBasic(ik_solver=self.ik, vc_context=self.vc)
+        self._nspg.setJointLimits(qmin, qmax)
 
     def set_validity_checker(self, vc):
         self.vc = vc
@@ -25,7 +26,7 @@ class GoalSampler:
     def sample(self, timeout):
         return self._nspg.sample(timeout)
 
-    def _generate_ik_cfg(self, links):
+    def _generate_ik_cfg(self, links, qmin, qmax):
         # write cartesio config
         cfg = dict()
 
@@ -62,8 +63,12 @@ class GoalSampler:
                 'distal_link': c,
             }
 
-        cfg['ee_E']['indices'] = [0, 1, 2, 3, 4, 5]
-        # cfg['ee_E']['base_link'] = 'base_link'
+        cfg['joint_limits']['limits'] = dict()
+        for i in range(self.model.getJointNum()):
+            jname = self.model.getEnabledJointNames()[i]
+            cfg['joint_limits']['limits'][jname] = [float(qmin[i]), float(qmax[i])]
+
+        cfg['ee_E']['indices'] = [0, 1, 2, 4, 5]
 
         return yaml.dump(cfg)
 
