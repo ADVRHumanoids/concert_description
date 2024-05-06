fac087a95c0d90a060a82fe69a1475f322677b1d
EdoardoRomiti <edoardo.romiti@iit.it>
2024-05-06 11:19:06 +0200

    upd `modular` python API. modular->v0.2.9

--- concert_examples/concert_bimanual_with_drill.py
+++ concert_examples/concert_bimanual_with_drill.py
@@ -40,42 +40,89 @@ with suppress_stdout():
     # manipulator
     data = urdf_writer.select_module_from_name('mobile_base_con5')
 
-    # J1
+    # Torso
+    data = urdf_writer.add_module('concert/module_Y_torso_concert.json', module_name='torso')
+
+    ##################################################################################
+    # arm 1
+    data = urdf_writer.select_module_from_name('torso_con2')
+
+    # J1 
     data = urdf_writer.add_module('concert/module_joint_yaw_A_concert.json')
     homing_joint_map[data['name']] = 0.0
 
-    # J2
+    # J2 
     data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
-    homing_joint_map[str(data['name'])] = -0.5
+    homing_joint_map[str(data['name'])] = 0.5
 
-    # add a 10cm passive link
-    #data = urdf_writer.add_module('concert/module_link_straight_100_concert.json')
-
-    # J3
+    # J3 
     data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
     homing_joint_map[data['name']] = 0.0
 
-    #add a 40cm passive link
+    # add a 40cm passive link
     data = urdf_writer.add_module('concert/module_link_straight_400_concert.json')
 
     # J4
     data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
-    homing_joint_map[str(data['name'])] = 0.5
+    homing_joint_map[str(data['name'])] = 1.9
 
     # J5
     data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
     homing_joint_map[str(data['name'])] = 0.0
 
-    #add a 40cm passive link
-    data = urdf_writer.add_module('concert/module_link_straight_400_concert.json')
+    # add a 40cm passive link
+    data = urdf_writer.add_module('concert/module_link_straight_300_concert.json')
 
     # J6
     data = urdf_writer.add_module('concert/module_joint_elbow_B_concert.json')
-    homing_joint_map[str(data['name'])] = -0.5
+    homing_joint_map[str(data['name'])] = 1.3
 
-    # gripper
-    #urdf_writer.add_simple_ee(0.0, 0.0, 0.2, 0.0)
-    data = urdf_writer.add_module('concert/passive_end_effector_panel.json')
+    # drill
+    urdf_writer.add_module('concert/module_drill_concert.json')
 
+    # drillbit
+    # urdf_writer.add_simple_ee(z_offset=0.30, radius=0.01, name='drillbit')
+    urdf_writer.add_drillbit(length=0.27, radius=0.012, mass=0.1)
+
+    ########################################################################
+    # arm 2
+    data = urdf_writer.select_module_from_name('torso_con3')
+
+    # J1 
+    data = urdf_writer.add_module('concert/module_joint_yaw_A_concert.json')
+    homing_joint_map[data['name']] = 0.0
+
+    # J2 
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = 0.5
+
+    # J3 
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[data['name']] = 0.0
+
+    # add a 40cm passive link
+    data = urdf_writer.add_module('concert/module_link_straight_400_concert.json')
+
+    # J4
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = 1.9
+
+    # J5
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[str(data['name'])] = 0.0
+
+    # add a 40cm passive link
+    data = urdf_writer.add_module('concert/module_link_straight_300_concert.json')
+
+    # J6
+    data = urdf_writer.add_module('concert/module_joint_elbow_B_concert.json')
+    homing_joint_map[str(data['name'])] = 1.3
+
+    # drill
+    urdf_writer.add_module('concert/module_drill_concert.json')
+
+    # drillbit
+    # urdf_writer.add_simple_ee(z_offset=0.30, radius=0.01, name='drillbit')
+    urdf_writer.add_drillbit(length=0.27, radius=0.012, mass=0.1)
 
 write_file_to_stdout(urdf_writer, homing_joint_map)
--- concert_examples/concert_with_drill.py
+++ concert_examples/concert_with_drill.py
@@ -7,78 +7,74 @@ with suppress_stdout():
     homing_joint_map = {}
 
     # add mobile base
-    urdf_writer.add_mobile_platform()
+    urdf_writer.add_module('concert/mobile_platform_concert.json', module_name='mobile_base')
 
     # leg + wheel 1
     data = urdf_writer.select_module_from_name('mobile_base_con1')
     wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
-                                        steering_filename='concert/module_steering_concert_fl_rr.json', 
-                                        angle_offset=0.0)
-    homing_joint_map[str(steering_data['lastModule_name'])] = {'angle': 0.0}
-    homing_joint_map[str(wheel_data['lastModule_name'])] = {'angle': 0.0}
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
 
     # leg + wheel 2
     data = urdf_writer.select_module_from_name('mobile_base_con2')
     wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
-                                        steering_filename='concert/module_steering_concert_fr_rl.json', 
-                                        angle_offset=0.0)
-    homing_joint_map[str(steering_data['lastModule_name'])] = {'angle': 0.0}
-    homing_joint_map[str(wheel_data['lastModule_name'])] = {'angle': 0.0}
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
 
     # leg + wheel 3
     data = urdf_writer.select_module_from_name('mobile_base_con3')
     wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
-                                        steering_filename='concert/module_steering_concert_fr_rl.json', 
-                                        angle_offset=0.0)
-    homing_joint_map[str(steering_data['lastModule_name'])] = {'angle': 0.0}
-    homing_joint_map[str(wheel_data['lastModule_name'])] = {'angle': 0.0}
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
 
     # leg + wheel 4
     data = urdf_writer.select_module_from_name('mobile_base_con4')
     wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
-                                        steering_filename='concert/module_steering_concert_fl_rr.json', 
-                                        angle_offset=0.0)
-    homing_joint_map[str(steering_data['lastModule_name'])] = {'angle': 0.0}
-    homing_joint_map[str(wheel_data['lastModule_name'])] = {'angle': 0.0}
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
 
     # manipulator
     data = urdf_writer.select_module_from_name('mobile_base_con5')
 
     # J1
-    data = urdf_writer.add_module('concert/module_joint_yaw_A_concert.json', 0, False)
-    homing_joint_map[data['lastModule_name']] = {'angle': 0.0}
+    data = urdf_writer.add_module('concert/module_joint_yaw_A_concert.json')
+    homing_joint_map[data['name']] = 0.0
 
     # J2
-    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json', 0, False)
-    homing_joint_map[str(data['lastModule_name'])] = {'angle': 0.5}
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = 0.5
 
     # add a 10cm passive link
     #
 
     # J3
-    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json', 0, False)
-    homing_joint_map[data['lastModule_name']] = {'angle': 0.0}
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[data['name']] = 0.0
 
     # add a 40cm passive link
-    data = urdf_writer.add_module('concert/module_link_straight_30_concert.json', 0, False)
+    data = urdf_writer.add_module('concert/module_link_straight_400_concert.json')
 
     # J4
-    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json', 0, False)
-    homing_joint_map[str(data['lastModule_name'])] = {'angle': 1.9}
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = 1.9
 
     # J5
-    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json', 0, False)
-    homing_joint_map[str(data['lastModule_name'])] = {'angle': 0.0}
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[str(data['name'])] = 0.0
 
     # add a 40cm passive link
-    data = urdf_writer.add_module('concert/module_link_straight_30_concert.json', 0, False)
+    data = urdf_writer.add_module('concert/module_link_straight_300_concert.json')
 
     # J6
-    data = urdf_writer.add_module('concert/module_joint_elbow_B_concert.json', 0, False)
-    homing_joint_map[str(data['lastModule_name'])] = {'angle': 1.3}
+    data = urdf_writer.add_module('concert/module_joint_elbow_B_concert.json')
+    homing_joint_map[str(data['name'])] = 1.3
 
     # drill
-    urdf_writer.add_module('concert/module_drill_concert.json', 0, False)
+    urdf_writer.add_module('concert/module_drill_concert.json')
 
     # drillbit
     # urdf_writer.add_simple_ee(z_offset=0.30, radius=0.01, name='drillbit')
