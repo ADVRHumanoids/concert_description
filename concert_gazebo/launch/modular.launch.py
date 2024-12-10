79d63a2fe291245fb53ad9fdaf8bd87f1cb2a3f8
JVALPASS <val1212.pass@gmail.com>
2024-12-10 16:31:09 +0100

    ros2 concert description implementation

---
+++ concert_gazebo/launch/modular.launch.py
@@ -0,0 +1,150 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command, TextSubstitution
+from launch.conditions import UnlessCondition
+from launch_ros.actions import Node
+import os
+from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Declare launch arguments
+    launch_arguments = [
+        DeclareLaunchArgument('gazebo', default_value='true'),
+        DeclareLaunchArgument('xbot2', default_value='true'),
+        DeclareLaunchArgument('xbot2_gui', default_value='true'),
+        DeclareLaunchArgument('xbot2_config', default_value=os.path.join(get_package_share_directory('concert_xbot2'), 'modular.yaml')),
+        DeclareLaunchArgument('modular_description', default_value=os.path.join(get_package_share_directory('concert_examples'), 'concert_example.py')),
+        DeclareLaunchArgument('realsense', default_value='false'),
+        DeclareLaunchArgument('velodyne', default_value='false'),
+        DeclareLaunchArgument('ultrasound', default_value='false'),
+        DeclareLaunchArgument('use_gpu_ray', default_value='false'),
+        DeclareLaunchArgument('paused', default_value='false'),
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
+        DeclareLaunchArgument('gui', default_value='true'),
+        DeclareLaunchArgument('headless', default_value='false'),
+        DeclareLaunchArgument('debug', default_value='false'),
+        DeclareLaunchArgument('physics', default_value='ode'),
+        DeclareLaunchArgument('verbose', default_value='false'),
+        DeclareLaunchArgument('extra_gazebo_args', default_value=''),
+        DeclareLaunchArgument('rviz', default_value='false'),
+        DeclareLaunchArgument('world_file', default_value=os.path.join(get_package_share_directory('concert_gazebo'), 'world/small_warehouse.sdf'))
+    ]
+
+
+    # Construct `gz_args` with conditional '-r' based on `paused`
+    gz_args = [
+        LaunchConfiguration('world_file'),
+        TextSubstitution(text=' '),
+        TextSubstitution(text='-v ') if LaunchConfiguration('verbose') == 'true' else TextSubstitution(text=''),
+        TextSubstitution(text='-s ') if LaunchConfiguration('gui') == 'false' else TextSubstitution(text=''),
+        TextSubstitution(text='-r') if UnlessCondition(LaunchConfiguration('paused')) else TextSubstitution(text='')
+    ]
+
+    # Robot description commands
+    robot_description_gz = Command([
+        'python3', ' ', LaunchConfiguration('modular_description'),
+        ' -o urdf -a gazebo_urdf:=true floating_base:=true',
+        ' realsense:=', LaunchConfiguration('realsense'),
+        ' velodyne:=', LaunchConfiguration('velodyne'),
+        ' ultrasound:=', LaunchConfiguration('ultrasound'),
+        ' use_gpu_ray:=', LaunchConfiguration('use_gpu_ray'),
+        ' -r modularbot_gz'
+    ],
+    on_stderr='ignore'
+    )
+
+    robot_description_xbot = Command([
+        'python3', ' ', LaunchConfiguration('modular_description'),
+        ' -o urdf -a gazebo_urdf:=false floating_base:=true',
+        ' realsense:=', LaunchConfiguration('realsense'),
+        ' velodyne:=', LaunchConfiguration('velodyne'),
+        ' ultrasound:=false',
+        ' use_gpu_ray:=', LaunchConfiguration('use_gpu_ray'),
+        ' -r modularbot'
+    ],
+    on_stderr='ignore'
+    )
+
+    robot_description_semantic = Command([
+        'python3', ' ', LaunchConfiguration('modular_description'),
+        ' -o srdf -a gazebo_urdf:=false',
+        ' realsense:=', LaunchConfiguration('realsense'),
+        ' velodyne:=', LaunchConfiguration('velodyne'),
+        ' ultrasound:=false',
+        ' use_gpu_ray:=', LaunchConfiguration('use_gpu_ray'),
+        ' -r modularbot'
+    ],
+    on_stderr='ignore'
+    )
+
+    # Gazebo group
+    gazebo_group = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
+            launch_arguments={'gz_args': gz_args}.items()
+        ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            name='urdf_spawner',
+            parameters=[{'string': robot_description_gz, 'z': 1.0}]
+        ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='ros_gz_bridge',
+            arguments=[
+                '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+                '/VLP16_lidar_back/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+                '/VLP16_lidar_front/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'
+            ],
+        )
+    ])
+
+    # Xbot2 process
+    xbot2_process = ExecuteProcess(
+        condition=IfCondition(LaunchConfiguration('xbot2')),
+        cmd=[
+            'xbot2-core', '-V', '--hw', 'sim', '--simtime',
+            '--config', LaunchConfiguration('xbot2_config'), '--'
+        ],
+        output='screen'
+    )
+
+    # Xbot2 GUI server and client
+    xbot2_gui_server = ExecuteProcess(
+        condition=IfCondition(LaunchConfiguration('xbot2_gui')),
+        cmd=['xbot2_gui_server', os.path.join(get_package_share_directory('concert_xbot2'), 'gui_config.yaml')],
+        output='log'
+    )
+
+    xbot2_gui_client = ExecuteProcess(
+        condition=IfCondition(LaunchConfiguration('xbot2_gui')),
+        cmd=['xbot2_gui'],
+        output='log'
+    )
+
+    # RViz node
+    rviz_node = Node(
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory('concert_gazebo'), 'rviz/concert_sensors.rviz')],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    # Create and return launch description
+    return LaunchDescription(launch_arguments + [
+        gazebo_group,
+        xbot2_process,
+        xbot2_gui_server,
+        xbot2_gui_client,
+        rviz_node
+    ])
---
+++ concert_odometry_ros2/launch/concert_odometry.launch.py
@@ -0,0 +1,112 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    
+    #package_dir = get_package_share_directory('concert_odometry_ros2')
+
+    # Define the configuration paths
+    config_dir = os.path.join(get_package_share_directory('concert_odometry_ros2'))
+    base_estimation_config = os.path.join(config_dir, 'base_estimation_params.yaml')
+    odometry_stack_config = os.path.join(config_dir, 'concert_odometry_stack.yaml')
+    odometry_config = os.path.join(config_dir, 'concert_odometry.yaml')
+    rviz_config = os.path.join(config_dir, 'rviz', 'concert_odometry.rviz')
+
+
+
+    # Launch arguments
+    publish_ground_truth_arg  = DeclareLaunchArgument(
+        'publish_ground_truth',
+        default_value='false',
+        description='Enable publishing of ground truth from Gazebo to TF'
+    )
+
+    gui_arg = DeclareLaunchArgument(
+        'gui',
+        default_value='false',
+        description='Enable GUI visualization with RViz'
+    )
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time (useful for Gazebo simulations)'
+    )
+
+    publish_ground_truth = LaunchConfiguration("publish_ground_truth")
+    gui = LaunchConfiguration("gui")
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+
+    # Load the file contents as a string
+    with open(odometry_stack_config, 'r') as file:
+        ik_problem_content = file.read()
+
+    # Main base estimation node with external YAML configuration file
+    base_estimation_node = Node(
+        package='base_estimation',
+        executable='base_estimation_node',
+        name='concert_odometry',
+        output='screen',
+        parameters=[
+                {'ik_problem': ik_problem_content},
+                base_estimation_config,  # Loading parameters from YAML file
+                {'world_from_tf': 'world' if publish_ground_truth == 'true' else {}},
+                odometry_config,  # Loading parameters from YAML fileV
+                {'use_sim_time': use_sim_time}
+        ],
+        remappings=[
+            ('/robot_description', '/xbotcore/robot_description'),
+            ('/robot_description_semantic', '/xbotcore/robot_description_semantic')
+        ]
+    )
+
+    # Publish ground truth from Gazebo to TF
+    gazebo_tf_publisher_node = Node(
+        condition=IfCondition(publish_ground_truth),
+        package='base_estimation',
+        executable='gazebo_tf_publisher',
+        name='gz_tf_publisher',
+        output='screen',
+        parameters=[
+            {'gz_link': 'ModularBot::base_link'},
+            {'tf_link': 'base_link'},
+            {'world': 'world'},
+            {'use_sim_time': use_sim_time}
+        ]
+    )
+
+    # Connect ground truth world to odometry world
+    static_transform_publisher_node = Node(
+        condition=IfCondition(publish_ground_truth),
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='connect_worlds',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'world', 'odometry/world', '10']
+    )
+
+    # RViz Node
+    rviz2_node = Node(
+        condition=IfCondition(gui),
+        package='rviz2',
+        executable='rviz2',
+        name='odom_rviz',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=['-d', rviz_config])  
+
+    return LaunchDescription([
+        publish_ground_truth_arg,
+        gui_arg,
+        use_sim_time_arg,
+        base_estimation_node,
+        gazebo_tf_publisher_node,
+        static_transform_publisher_node,
+        rviz2_node
+    ])
