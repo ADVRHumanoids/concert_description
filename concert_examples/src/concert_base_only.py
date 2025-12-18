38e5ea76de3cccc991bc25334a908d841c4db4b3
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-12-18 17:42:13 +0100

    concert base only

---
+++ concert_examples/src/concert_base_only.py
@@ -0,0 +1,42 @@
+from modular.URDF_writer import *
+
+with suppress_stdout():
+
+    # create UrdfWriter object and joint map to store homing values
+    urdf_writer = UrdfWriter(speedup=True, floating_base=True)
+    homing_joint_map = {}
+
+    # add mobile base
+    urdf_writer.add_module('concert/mobile_platform_concert.json', module_name='mobile_base')
+
+    # leg + wheel 1
+    data = urdf_writer.select_module_from_name('mobile_base_con1')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 2
+    data = urdf_writer.select_module_from_name('mobile_base_con2')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 3
+    data = urdf_writer.select_module_from_name('mobile_base_con3')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 4
+    data = urdf_writer.select_module_from_name('mobile_base_con4')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+
+
+write_file_to_stdout(urdf_writer, homing_joint_map)
