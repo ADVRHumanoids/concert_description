2f601d719cbb16afbcbdc44f5325ab4cc3c920e9
EdoardoRomiti <edoardo.romiti@iit.it>
2026-03-12 17:06:48 +0000

    re-added collaborative transportation example

---
+++ concert_examples/src/concert_collaborative_transportation.py
@@ -0,0 +1,98 @@
+from modular.URDF_writer import *
+
+with suppress_stdout():
+
+    # create UrdfWriter object and joint map to store homing values
+    urdf_writer = UrdfWriter(speedup=True, floating_base=True, verbose=False)
+    homing_joint_map = {}
+
+    # add mobile base
+    urdf_writer.add_module('concert/mobile_platform_concert.json', module_name='mobile_base')
+
+    # leg + wheel 1
+    data = urdf_writer.select_module_from_name('mobile_base_con1')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 2
+    data = urdf_writer.select_module_from_name('mobile_base_con2')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 3
+    data = urdf_writer.select_module_from_name('mobile_base_con3')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fr_rl.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # leg + wheel 4
+    data = urdf_writer.select_module_from_name('mobile_base_con4')
+    wheel_data, steering_data = urdf_writer.add_wheel_module(wheel_filename='concert/module_wheel_concert.json', 
+                                        steering_filename='concert/module_steering_concert_fl_rr.json')
+    homing_joint_map[str(steering_data['name'])] = 0.0
+    homing_joint_map[str(wheel_data['name'])] = 0.0
+
+    # manipulator
+    data = urdf_writer.select_module_from_name('mobile_base_con5')
+
+    # J1
+    data = urdf_writer.add_module('concert/module_joint_yaw_A_concert.json')
+    homing_joint_map[data['name']] = 0.0
+
+    # J2
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = -0.5
+
+    # add a 10cm passive link
+    #
+
+    # J3
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[data['name']] = 0.0
+
+    # add a 40cm passive link
+    data = urdf_writer.add_module('concert/module_link_straight_400_concert.json')
+
+    # J4
+    data = urdf_writer.add_module('concert/module_joint_elbow_A_concert.json')
+    homing_joint_map[str(data['name'])] = 1.9
+
+    # J5
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[str(data['name'])] = 0.0
+
+    # # add a 40cm passive link
+    # data = urdf_writer.add_module('concert/module_link_straight_300_concert.json')
+
+    # J6
+    data = urdf_writer.add_module('concert/module_joint_elbow_B_concert.json')
+    homing_joint_map[str(data['name'])] = -2.0
+
+    data = urdf_writer.add_module('concert/module_joint_yaw_B_concert.json')
+    homing_joint_map[str(data['name'])] = 0.0
+
+    # drill
+    # urdf_writer.add_module('concert/module_drill_concert.json')
+    # urdf_writer.add_module('concert/module_spraying_tool_concert.json')
+
+    urdf_writer.add_module('module_dagana.json')
+    homing_joint_map['dagana_E_claw_joint'] = 1.0
+    urdf_writer.add_dagana_claws('concert_formwork_claws')
+
+    # drillbit
+    # urdf_writer.add_simple_ee(z_offset=0.30, radius=0.01, name='drillbit')
+    # urdf_writer.add_drillbit(length=0.27, radius=0.012, mass=0.1)
+
+write_file_to_stdout(urdf_writer, homing_joint_map)
+
+# # content = urdf_writer.process_urdf()
+# # # open(f'/tmp/modularbot.urdf', 'w').write(content)
+# # urdf_writer.write_urdf()
+# # urdf_writer.write_srdf(homing_joint_map)
+# urdf_writer.write_lowlevel_config()
+
